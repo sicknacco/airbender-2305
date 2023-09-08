5865492a688f86c0248630ad2babe9b2b8253897
Nick Sacco <nicksacco10@gmail.com>
2023-09-08 08:06:23 -0700

    set up service with tests and begin feature tests

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Search Index Page' do
+  describe 'As a user' do
+    it 'I can search for members of a nation', :vcr do
+      visit '/'
+
+      select 'Fire Nation', from: :nation
+      click_button 'Search For Members'
+
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content('Total Members:')
+    end
+  end
+end
---
+++ spec/services/avatar_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe AvatarService do
+  describe '#nation_members' do
+    it 'returns a list of members of a nation', :vcr do
+      characters = AvatarService.nation_members('fire nation', 1)
+
+      expect(characters).to be_an(Array)
+      characters.each do |character|
+        expect(character).to have_key(:_id)
+        expect(character[:_id]).to be_a(String)
+        expect(character).to have_key(:allies)
+        expect(character[:allies]).to be_an(Array)
+        expect(character).to have_key(:enemies)
+        expect(character[:enemies]).to be_an(Array)
+        expect(character).to have_key(:name)
+        expect(character[:name]).to be_a(String)
+        expect(character).to have_key(:affiliation)
+        expect(character[:affiliation]).to be_a(String)
+      end
+    end
+  end
+end
